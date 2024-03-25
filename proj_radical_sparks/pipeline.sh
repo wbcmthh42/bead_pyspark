c240de10d95c50f46cef22bd0953a968baa48b2b
Johnny Tay <tayhhjohnny@outlook.com>
2024-03-25 23:09:35 +0800

    add script to include simulated data in mysql

--- proj_radical_sparks/pipeline.sh
+++ proj_radical_sparks/pipeline.sh
@@ -2,6 +2,7 @@
 
 python pull_reddit_submissions.py
 python save_in_mysql.py
+python save_simulated_data_in_mysql.py
 python retrieve_with_pyspark.py
 python data_processing.py
 python llm_classification.py
--- proj_radical_sparks/save_simulated_data_in_mysql.py
+++ proj_radical_sparks/save_simulated_data_in_mysql.py
@@ -15,35 +15,29 @@ class load_to_mysql():
         self.env_file_path = env_file_path
         pass
 
-    def parquet_to_df(self, parquet_folder):
+    def csv_to_df(self, csv_file):
         """
         Read Parquet files from the specified folder and its subfolders, and concatenate the data into a single DataFrame.
-        
+
         Parameters:
             parquet_folder (str): The path to the folder containing Parquet files.
-        
+
         Returns:
             pandas.DataFrame: A DataFrame containing the combined data from all the Parquet files.
         """
 
         # Get a list of all Parquet files in the folder and its subfolders
-        parquet_files = glob.glob(os.path.join(parquet_folder, '**/*.parquet'), recursive=True)
+        self.simulated_df = pd.read_csv(csv_file)
 
-        # Read each Parquet file and append the data to a list
-        data = []
-        for file in parquet_files:
-            table = pq.read_table(file)
-            df = table.to_pandas()
-            data.append(df)
+        self.simulated_df['timestamp'] = pd.to_datetime(self.simulated_df['timestamp'], format='%d/%m/%y %H:%M')
+        self.simulated_df['date'] = pd.to_datetime(self.simulated_df['date'], format='%d/%m/%y')
 
-        # Concatenate all the data into a single DataFrame
-        self.df_combined = pd.concat(data, ignore_index=True)
+        return self.simulated_df
 
-        return self.df_combined
 
     def save_to_mysql(self):
         """
-        Save data to a MySQL database using environment variables from a .env file. 
+        Save data to a MySQL database using environment variables from a .env file.
         """
 
         # Load environment variables from .env file
@@ -60,25 +54,21 @@ class load_to_mysql():
             user=db_user,
             passwd=db_password,
             database=db_database
-            )
+        )
 
         mycursor = mydb.cursor()
 
-        engine = create_engine('mysql+pymysql://user:passwd@host/database')
-        # Create the table
-        mycursor.execute(
-            "CREATE TABLE IF NOT EXISTS proj_radical_sparks (submission_id VARCHAR(255), comment_id VARCHAR(255), timestamp TIMESTAMP, author VARCHAR(255), body TEXT, submission TEXT, upvotes VARCHAR(255), upvote_ratio VARCHAR(255), date DATE)"
-        )
+        mycursor.execute("CREATE TABLE IF NOT EXISTS proj_radical_sparks (submission_id VARCHAR(255), comment_id VARCHAR(255), timestamp TIMESTAMP, author VARCHAR(255), body TEXT, submission TEXT, upvotes VARCHAR(255), upvote_ratio VARCHAR(255), date DATE);")
 
         sqlFormula = "INSERT INTO proj_radical_sparks (submission_id, comment_id, timestamp, author, body, submission, upvotes, upvote_ratio, date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE submission_id=VALUES(submission_id), comment_id=VALUES(comment_id), timestamp=VALUES(timestamp), author=VALUES(author), body=VALUES(body), submission=VALUES(submission), upvotes=VALUES(upvotes), upvote_ratio=VALUES(upvote_ratio), date=VALUES(date);"
 
         # Insert DataFrame data into the MySQL table
-        mycursor.executemany(sqlFormula, self.df_combined.values.tolist())
+        mycursor.executemany(sqlFormula, self.simulated_df.values.tolist())
 
         mydb.commit()
 
 
 if __name__ == "__main__":
-    tomysql = load_to_mysql('.env')
-    tomysql.parquet_to_df('./reddit_data_folder/')
-    tomysql.save_to_mysql()
+    csvmysql = load_to_mysql('.env')
+    csvmysql.csv_to_df('labelled_radical_data.csv')
+    csvmysql.save_to_mysql()
